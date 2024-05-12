14fb989b9ca7ec700ba7cdfe134e75e01cfe922b
JD-MBP <jashansingh29@gmail.com>
2024-05-12 02:21:18 -0400

    d

--- T01homepage/views.py
+++ T01homepage/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(requests):
-    return render(requests,'homepage/main.html')
+    data={'askForEmail': True}
+    return render(requests,'homepage/main.html',data)
+
+
